86c60f53ba62035e1c495d158662077e7b6cd177
ben519 <bgorman519@gmail.com>
2024-08-05 20:52:47 -0500

    class tweak

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import { Select } from '@/components/select'
 
 export default async function Home() {
   return (
-    <Select name="period" className="w-48">
+    <Select name="period" className="max-w-48">
       <option value="last_week">Last week</option>
       <option value="last_two">Last two weeks</option>
       <option value="last_month">Last month</option>
